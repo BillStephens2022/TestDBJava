195f8300bc816be4b7b9b163ac1d40dbd234bcd8
BillStephens2022 <two4onebill@yahoo.com>
2024-01-28 10:20:02 -0500

    successfully created table and added record to the table in the database

--- src/com/billstephens/Main.java
+++ src/com/billstephens/Main.java
@@ -3,6 +3,7 @@ package com.billstephens;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,6 +12,14 @@ public class Main {
              if (conn != null) {
                  System.out.println("Connection established successfully.");
              }
+             Statement statement = conn.createStatement();
+             statement.execute("CREATE TABLE IF NOT EXISTS contacts" +
+                     " (name TEXT, phone INTEGER, email TEXT)");
+             statement.execute("INSERT INTO contacts (name, phone, email) " +
+                     "VALUES('Bill', 9876543210, 'bill@gmail.com')");
+             statement.close();
+             conn.close();
+
          } catch(SQLException e) {
              System.out.println("Something went wrong: " + e.getMessage());
          }
